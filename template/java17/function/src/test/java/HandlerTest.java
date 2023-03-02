92b5b3677d8b333a182b09f2b533082d7c82f43b
fabrizzz <giampietro.fabriziobonaccorsi@gmail.com>
2023-03-02 03:48:21 +0100

    Sample test upgraded

--- template/java17/function/src/test/java/HandlerTest.java
+++ template/java17/function/src/test/java/HandlerTest.java
@@ -7,6 +7,6 @@ import com.openfaas.model.IHandler;
 public class HandlerTest {
     @Test public void handlerIsNotNull() {
         IHandler handler = new Handler();
-        assertTrue("Expected handler not to be null", handler != null);
+        assertNotNull("Expected handler not to be null", handler);
     }
 }
